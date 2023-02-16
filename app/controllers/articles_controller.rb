dc460d09c77019b5db3f7d426a71eb8d5821fc29
Tom <tom.lebeau19@gmail.com>
2023-02-16 12:34:03 -0500

    Added validation tocreate flow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,17 @@ class ArticlesController < ApplicationController
   def create
     # This includes whitelisting the params that are passed into the method
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    #redirect_to action: 'index'
-    #redirect_to article_path(@article)
-    redirect_to @article
+    if @article.save
+      #redirect_to action: 'index'
+     #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+    
   end
   
   def new
-    
+    @article = Article.new
   end
 end
