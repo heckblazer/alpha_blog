84e5d2fafc26f0bc67da37319482ef1dd721c51b
Tom <tom.lebeau19@gmail.com>
2023-02-16 12:21:53 -0500

    updated create new feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def create
+    # This includes whitelisting the params that are passed into the method
     @article = Article.new(params.require(:article).permit(:title, :description))
     @article.save
     #redirect_to action: 'index'
-    redirect_to article_path(@article)
+    #redirect_to article_path(@article)
+    redirect_to @article
   end
   
   def new
